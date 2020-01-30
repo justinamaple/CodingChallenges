c230ac092c417570348a9de940c49dbde01a5860
Justin Maple <justinamaple@gmail.com>
2020-01-29 16:26:13 -0800

    Updated TwoSum Comments

--- Ruby/TwoSum.rb
+++ Ruby/TwoSum.rb
@@ -3,8 +3,14 @@
 #   Use a hash to store the index of the current number at complement value,
 #   which is found by subtracting the current num from the target. If the
 #   current value exists, return the index stored there plus the current index.
-# Time Complexity: O(n), the array will only need to be traversed once.
-# Space Complexity: O(n), the hash used will not exceed the size of the array.
+# Time Complexity:
+#   O(n), the array will only need to be traversed once.
+# Space Complexity:
+#   O(n), the hash used will not exceed the size of the array.
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
 def two_sum(nums, target)
   hash = {}
 
