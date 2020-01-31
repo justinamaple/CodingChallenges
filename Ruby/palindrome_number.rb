fc616da60b42da0773233c35a99d7126fc659426
Justin Maple <justinamaple@gmail.com>
2020-01-31 14:31:45 -0800

    Added PalindromeNumber

---
+++ Ruby/palindrome_number.rb
@@ -0,0 +1,49 @@
+# Source:
+#   https://leetcode.com/problems/palindrome-number/
+# Solution:
+#   Start with the edge cases, if the number is negative return
+#   false. Several solutions are given, the first was the most
+#   obvious, where you just turn it into a string and reverse it.
+#   This can be improved by instead of checking the entire string,
+#   checking the digits from out in and making sure they align.
+#
+#   Lastly, I included a solution which creates the reverse integer
+#   mathematically, however this is actually quite a bit slower.
+# Time Complexity:
+#   O(n), the array will only need to be traversed once.
+# Space Complexity:
+#   O(1)
+
+# @param {Integer} x
+# @return {Boolean}
+def is_palindrome(x)
+  str = x.to_s
+  str.reverse == str
+end
+
+def is_palindrome1(x)
+  return false if x.negative?
+  
+  digits = x.digits
+  
+  (digits.length / 2).times do |i|
+    return false if digits[i] != digits[-i - 1] 
+  end
+  
+  true
+end
+
+def is_palindrome2(x)
+  return false if (x.negative? || (x != 0 && (x % 10).zero?))
+
+  reverse = 0
+  # This only requires us to go half way, avoiding
+  # potential issues with overflow.
+  while x > reverse
+    reverse = reverse * 10 + x % 10
+    x /= 10
+  end
+
+  # Even and Odd Cases
+  x == reverse || x == reverse / 10
+end
