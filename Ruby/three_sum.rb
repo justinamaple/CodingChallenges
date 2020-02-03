3645306db507e0261bd06bfaaddc8589d6d65776
Justin Maple <justinamaple@gmail.com>
2020-02-03 15:53:10 -0800

    Added ThreeSum

---
+++ Ruby/three_sum.rb
@@ -0,0 +1,43 @@
+# Source:
+#   https://leetcode.com/problems/3sum/
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def three_sum(nums)
+  return [] if nums.length < 3
+
+  solutions = []
+  nums.sort!
+
+  (0..nums.length - 2).each do |i|
+    # Since nums is sorted solution 
+    # cannot be made of 3 positive vals
+    break if nums[i].positive?
+    # Zip past duplicate start vals
+    next if i > 0 && nums[i] == nums[i - 1]
+
+    left = i + 1
+    right = nums.length - 1
+
+    while left < right
+      sum = nums[i] + nums[left] + nums[right]
+
+      if sum.zero?
+        solutions.push([nums[i], nums[left], nums[right]])
+
+        # Zip past duplicate solutions
+        left += 1 while nums[left] == nums[left + 1]
+        right -= 1 while nums[right] == nums[right - 1]
+
+        left += 1
+        right -= 1
+      elsif sum.negative?
+        left += 1
+      else # sum.positive?
+        right -= 1
+      end
+    end
+  end
+
+  solutions
+end
