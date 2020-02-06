81f2b036aa7f13ca6add81e68e22610effe572e2
Justin Maple <justinamaple@gmail.com>
2020-02-06 15:22:17 -0800

    Updated PalindromeNumber Solution and Comments

--- Ruby/palindrome_number.rb
+++ Ruby/palindrome_number.rb
@@ -1,45 +1,43 @@
 # Source:
 #   https://leetcode.com/problems/palindrome-number/
-<<<<<<< HEAD
 # Solution:
-#   Start with the edge cases, if the number is negative return
-#   false. Several solutions are given, the first was the most
-#   obvious, where you just turn it into a string and reverse it.
-#   This can be improved by instead of checking the entire string,
-#   checking the digits from out in and making sure they align.
+#   Start with the edge cases, if the number is negative or it is
+#   divisible by 10 and not zero (think 1000) return false. Several
+#   solutions are given, the simplest is where you just turn it into a
+#   string and reverse it. The proper math way requires you to create
+#   a reversed number, multiplying it by 10 everytime you pull of a digit.
 #
-#   Lastly, I included a solution which creates the reverse integer
-#   mathematically, however this is actually quite a bit slower.
+#   The trick is to be careful of sign overflow, since positive values
+#   cannot be as large as their negative counterparts. To avoid this,
+#   you actually only need to check half of the number, so if reverse
+#   becomes larger then x stop. Then you must check if they are equal,
+#   or in the odd case reverse/10 is equal.
 # Time Complexity:
-#   O(n), the array will only need to be traversed once.
+#   O(n), only half of the array will need to be traversed.
 # Space Complexity:
 #   O(1)
 
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-  str = x.to_s
-  str.reverse == str
-end
+  return false if x.negative? || (x != 0 && (x % 10).zero?)
 
-def is_palindrome1(x)
-  return false if x.negative?
-  
-  digits = x.digits
-  
-  (digits.length / 2).times do |i|
-    return false if digits[i] != digits[-i - 1] 
+  reverse = 0
+  # This only requires us to go half way, avoiding
+  # potential issues with overflow.
+  while x > reverse
+    reverse = reverse * 10 + x % 10
+    x /= 10
   end
-  
-  true
+
+  # Even and Odd Cases
+  x == reverse || x == reverse / 10
 end
 
-def is_palindrome2(x)
-  return false if (x.negative? || (x != 0 && (x % 10).zero?))
-=======
-
-# @param {Integer} x
-# @return {Boolean}
+# This takes advantage of turning the problem into a string.
+# Most likely the fastest and most efficient way, but not as
+# simple in older languages, and doesn't show mastery of math.
+# Ask which solution they would prefer!
 def is_palindrome_str(x)
   return false if x.negative?
 
@@ -52,21 +50,10 @@ def is_palindrome_str(x)
   true
 end
 
-def is_palindrome_int(x)
-  return false if x.negative? || (x != 0 && (x % 10).zero?)
->>>>>>> HomePC
-
-  reverse = 0
-  # This only requires us to go half way, avoiding
-  # potential issues with overflow.
-  while x > reverse
-    reverse = reverse * 10 + x % 10
-    x /= 10
-  end
-
-<<<<<<< HEAD
-  # Even and Odd Cases
-=======
->>>>>>> HomePC
-  x == reverse || x == reverse / 10
+# Silly ruby one liner, surprisingly fast and clear.
+# Might be faster breaking into two lines so that to_s
+# is only called once, since in actuality this is
+# executing 4+ commands to the cpu.
+def is_palindrome_one_liner(x)
+  x.to_s.reverse == x.to_s
 end
