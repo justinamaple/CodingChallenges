32adfdcfaef2868d1dc26c2d0e24204cafab1bff
Justin Maple <justinamaple@gmail.com>
2020-02-10 16:50:22 -0800

    Updated Solution Description for ThreeSumClosest

--- Ruby/three_sum_closest.rb
+++ Ruby/three_sum_closest.rb
@@ -1,3 +1,23 @@
+# Source:
+#   https://leetcode.com/problems/3sum-closest/
+# Solution:
+#   Begin with a guard clause, if there are less then three values
+#   then it is not possible to have a 3sum, return 0. In order
+#   to accurately use the array, it must be sorted. Iterate over the
+#   array - 2 (since the index of i < left < right).
+#
+#   Next check for duplicated a values, and zip past them by increasing
+#   the index where they are equal. Now that we have a unique value,
+#   set b to the left most, and c to the right most possible value.
+#   While left < right, check the value of the sum. If the absolute value
+#   of the difference between the sum and target is smaller than that of
+#   the closest and target, update closest. Finally return the solution set.
+# Time Complexity:
+#   O(n^2), For each value in the array, every other value will be checked
+#   using the left and right pointers, one at a time.
+# Space Complexity:
+#   O(1), no extra space used.
+
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer}
