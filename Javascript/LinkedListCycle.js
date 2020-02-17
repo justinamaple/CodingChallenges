aeeb813fffaa10ab347ac4d07928c4891a63ec2c
Justin Maple <justinamaple@gmail.com>
2020-02-17 14:07:40 -0800

    Updated LinkedListCycle Solution

--- Javascript/LinkedListCycle.js
+++ Javascript/LinkedListCycle.js
@@ -1,3 +1,17 @@
+// Source:
+//   https://leetcode.com/problems/linked-list-cycle/
+// Solution:
+//   Guard clause to return if list is empty. Otherwise set
+//   two pointers to the head, one fast and one slow. The slow
+//   goes one point at a time, while the fast goes two at a time
+//   If fast and slow ever point to the same node then there is a
+//   cycle. Otherwise slow will traverse the entire list and hit a
+//   null value, in which case you return false.
+// Time Complexity:
+//   O(n)
+// Space Complexity:
+//   O(1), no extra data structures used.
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
