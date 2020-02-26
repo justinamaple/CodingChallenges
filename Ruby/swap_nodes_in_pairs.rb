6318689244e77f870d942be48de6af61fa38c5b9
Justin Maple <justinamaple@gmail.com>
2020-02-25 17:17:17 -0800

    Added alternate solution to SwapNodesInPairs

--- Ruby/swap_nodes_in_pairs.rb
+++ Ruby/swap_nodes_in_pairs.rb
@@ -9,6 +9,35 @@
 
 # @param {ListNode} head
 # @return {ListNode}
+def swap_pairs(head)
+  current = head
+  two_ago = nil
+  last = nil
+  i = 1
+
+  until current.nil?
+    if (i % 2).zero?
+      temp = current.next
+      current.next = last
+      last.next = temp
+
+      if i < 3
+        head = current
+      else
+        two_ago.next = current
+      end
+
+      two_ago = last
+    else
+      last = current
+    end
+
+    current = last.next
+    i += 1
+  end
+
+  head
+end
 
 # This solution could definitely be more space efficient since the
 # array isn't really needed, just the last 3 variables.
