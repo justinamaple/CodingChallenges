40dbc939758705ab3017a312281af5b89f468e1c
Justin Maple <justinamaple@gmail.com>
2020-02-27 17:08:09 -0800

    Holy MOLY that is a clean ValidSudoku check

--- Ruby/valid_sudoku.rb
+++ Ruby/valid_sudoku.rb
@@ -1,3 +1,20 @@
+# Source:
+#   https://leetcode.com/problems/valid-sudoku/
+# Solution:
+#   Checks from top left to bottom right each piece of the board.
+#   Creates an array of size 3 to each carry an array of size 9,
+#   each with a set to signify a row, column, or sub_box. As the
+#   board is being checked, the sectory is calculated and the
+#   value is added to the seen sets. If the value already exists,
+#   add? returns nil and the whole returns false.
+#
+#  Slight speed optimization by not calling the check if the num
+#  is '.', which stands for an empty square.
+# Time Complexity:
+#   O(1), technically no more then 81 squares will need to be checked.
+# Space Complexity:
+#   O(1), the space used is also constant.
+
 # @param {Character[][]} board
 # @return {Boolean}
 # For use to access the correct sub array of seen
@@ -7,26 +24,19 @@ BOXS = 2
 
 def is_valid_sudoku(board)
   # 0,0 is top left. 8,8 bot right for the board
-  seen = Array.new(3) { |_| Array.new(9) { Set.new } }
+  seen = Set.new
 
   9.times do |y|
     9.times do |x|
-      sector = (y / 3) * 3 + (x / 3)
       next if board[y][x] == '.'
-      return false if already_seen?(x, y, sector, board, seen)
+
+      sector = (y / 3) * 3 + (x / 3).to_s
+      num = board[y][x].to_s
+      return false unless seen.add?(num + ' in row ' + y) \
+                       && seen.add?(num + ' in col ' + x) \
+                       && seen.add?(num + ' in box ' + sector)
     end
   end
 
   true
-end
-
-# Returns true if the board is no longer valid
-# and false if it is still valid.
-def already_seen?(x, y, sector, board, seen)
-  num = board[y][x]
-  return true unless seen[ROWS][y].add?(num) \
-                  && seen[COLS][x].add?(num) \
-                  && seen[BOXS][sector].add?(num)
-
-  false
 end
