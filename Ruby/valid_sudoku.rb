fcc817275ed95a0304449c7173ce37b62c2957fb
Justin Maple <justinamaple@gmail.com>
2020-02-27 17:14:04 -0800

    Cleaned up ValidSudoku to use Interpolation

--- Ruby/valid_sudoku.rb
+++ Ruby/valid_sudoku.rb
@@ -2,11 +2,8 @@
 #   https://leetcode.com/problems/valid-sudoku/
 # Solution:
 #   Checks from top left to bottom right each piece of the board.
-#   Creates an array of size 3 to each carry an array of size 9,
-#   each with a set to signify a row, column, or sub_box. As the
-#   board is being checked, the sectory is calculated and the
-#   value is added to the seen sets. If the value already exists,
-#   add? returns nil and the whole returns false.
+#   Creates a set which uses a string as the key, which is the
+#   location written out in plain english.
 #
 #  Slight speed optimization by not calling the check if the num
 #  is '.', which stands for an empty square.
@@ -17,11 +14,6 @@
 
 # @param {Character[][]} board
 # @return {Boolean}
-# For use to access the correct sub array of seen
-ROWS = 0
-COLS = 1
-BOXS = 2
-
 def is_valid_sudoku(board)
   # 0,0 is top left. 8,8 bot right for the board
   seen = Set.new
@@ -30,11 +22,11 @@ def is_valid_sudoku(board)
     9.times do |x|
       next if board[y][x] == '.'
 
-      sector = (y / 3) * 3 + (x / 3).to_s
+      sector = (y / 3) * 3 + (x / 3)
       num = board[y][x].to_s
-      return false unless seen.add?(num + ' in row ' + y) \
-                       && seen.add?(num + ' in col ' + x) \
-                       && seen.add?(num + ' in box ' + sector)
+      return false unless seen.add?("#{num} in row #{y}") \
+                       && seen.add?("#{num} in col #{x}") \
+                       && seen.add?("#{num} in box #{sector}")
     end
   end
 
