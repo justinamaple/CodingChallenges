86444c11a82e1d612f6280c6234ee975a328bda9
Justin Maple <justinamaple@gmail.com>
2020-03-07 11:33:40 -0800

    Added GradingStudents

---
+++ Hackerrank/Ruby/grading_students.rb
@@ -0,0 +1,40 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades)
+  grades.map { |grade| round_up(grade) }
+end
+
+def round_up(grade)
+  remainder = grade % 5
+  return grade if grade < 38 || remainder < 3
+  
+  grade + 5 - remainder
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+grades_count = gets.strip.to_i
+
+grades = Array.new(grades_count)
+
+grades_count.times do |i|
+    grades_item = gets.strip.to_i
+    grades[i] = grades_item
+end
+
+result = gradingStudents grades
+
+fptr.write result.join "\n"
+fptr.write "\n"
+
+fptr.close()
