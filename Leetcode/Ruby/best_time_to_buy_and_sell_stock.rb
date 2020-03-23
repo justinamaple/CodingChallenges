555fb2e9f83a13e4d02101251ad41d9f26b09b9f
Justin Maple <justinamaple@gmail.com>
2020-03-22 18:06:49 -0700

    Updated BestTimeToBuyAndSellStock

--- Leetcode/Ruby/best_time_to_buy_and_sell_stock.rb
+++ Leetcode/Ruby/best_time_to_buy_and_sell_stock.rb
@@ -1,30 +1,16 @@
+# Source:
+#   https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-  # Should make a class for MIN_INT and MAX_INT
-  max = -9999999
-  min = 9999999
-  profit = 0
+  max = 0
+  min = prices[0]
 
-  # Store Min Before Index, and Max After Index
-  hash = Hash.new({})
-
-  # Fill in Min
-  (0...prices.length).each do |index|
-    min = [prices[index], min].min
-    hash[index] = { min: min }
+  (1...prices.length).each do |i|
+    min = [prices[i], min].min
+    max = [prices[i] - min, max].max
   end
 
-  # Fill in Max
-  (0...prices.length).reverse_each do |index|
-    max = [prices[index], max].max
-    hash[index][:max] = max
-  end
-
-  # Find max profit
-  hash.each_value do |h|
-    profit = [h[:max] - h[:min], profit].max
-  end
-
-  profit
+  max
 end
