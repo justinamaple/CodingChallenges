85c89466d938cea68488ef4e22f1b8fa5a07e089
Justin Maple <justinamaple@gmail.com>
2020-03-30 12:27:36 -0700

    Added GetIntersectionNode

---
+++ Leetcode/Ruby/intersection_of_two_linked_lists.rb
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} headA
+# @param {ListNode} headB
+# @return {ListNode}
+def getIntersectionNode(headA, headB)
+  hash = {}
+
+  current = headA
+  while current
+    hash[current] = true
+    current = current.next
+  end
+
+  current = headB
+  while current
+    return current if hash[current]
+
+    current = current.next
+  end
+
+  nil
+end
