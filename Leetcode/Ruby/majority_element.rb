513144bb5b69e35e556623db85f3553e0cb7665f
Justin Maple <justinamaple@gmail.com>
2020-04-02 18:32:06 -0700

    Updated MajorityElement

--- Leetcode/Ruby/majority_element.rb
+++ Leetcode/Ruby/majority_element.rb
@@ -12,3 +12,14 @@ def majority_element(nums)
     return num if freq[num] > nums.length / 2
   end
 end
+
+# Potentially faster since the if in the loop could be costly
+def majority_element(nums)
+  freq = Hash.new(0)
+  nums.each { |num| freq[num] += 1 }
+
+  half_len =  nums.length / 2
+  max = freq.select { |key, val| val > half_len }
+
+  max.keys.first
+end
