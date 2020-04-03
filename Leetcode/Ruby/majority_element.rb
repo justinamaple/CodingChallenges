b19ba8cd1b31ed56b4d9d5f5156ce73674b86117
Justin Maple <justinamaple@gmail.com>
2020-04-02 18:25:16 -0700

    Added majority element

---
+++ Leetcode/Ruby/majority_element.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def majority_element(nums)
+  freq = Hash.new(0)
+
+  nums.each do |num|
+    freq[num] += 1
+
+    return num if freq[num] > nums.length / 2
+  end
+end
