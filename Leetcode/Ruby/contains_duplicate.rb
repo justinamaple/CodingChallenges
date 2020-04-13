1f2439f584d09c2df5f68c2a1271172029f5bc88
Justin Maple <justinamaple@gmail.com>
2020-04-12 20:26:19 -0700

    Added conatins Duplicates

---
+++ Leetcode/Ruby/contains_duplicate.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+  freq = Hash.new(0)
+
+  nums.each do |num|
+    freq[num] += 1
+    return true if freq[num] >= 2
+  end
+
+  false
+end
