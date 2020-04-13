1fed1523e5719c6ee11cec5e7f6498a44a094354
Justin Maple <justinamaple@gmail.com>
2020-04-12 20:21:44 -0700

    Added recursive ReverseLinkedList

--- Leetcode/Ruby/reverse_linked_list.rb
+++ Leetcode/Ruby/reverse_linked_list.rb
@@ -25,3 +25,33 @@ def reverse_list(head)
 
   last
 end
+
+# Recursive version
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  current = head
+  last = nil
+
+  reverse_node(head, current, last)
+end
+
+def reverse_node(head, current, last)
+  return last if current.nil?
+
+  temp = current.next
+  current.next = last
+  last = current
+  current = temp
+
+  reverse_node(head, current, last)
+end
