8bc8626dd9b89e4119968f3315c83051bc3a7475
Justin Maple <justinamaple@gmail.com>
2020-04-17 20:07:31 -0700

    Added GoatLatin

---
+++ Leetcode/Ruby/goat_latin.rb
@@ -0,0 +1,28 @@
+# @param {String} s
+# @return {String}
+def to_goat_latin(s)
+  words = s.split(' ')
+  
+  words.each.with_index do |word, i|
+    if vowel?(word[0])
+      words[i] = word + 'ma' + 'a' * (i + 1)
+    else
+      words[i] = word[1..-1] + word[0] + 'ma' + 'a' * (i + 1)
+    end
+  end
+  
+  words.join(' ')
+end
+
+def vowel?(char)
+  # And sometimes y?
+  vowels = { 
+    'a' => true,
+    'e' => true,
+    'i' => true,
+    'o' => true,
+    'u' => true
+  }
+  
+  vowels[char.downcase]
+end
