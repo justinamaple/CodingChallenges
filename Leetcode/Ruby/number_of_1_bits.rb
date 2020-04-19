2a6b3c20cf708b8ff38df761f4856bd5f2e20455
Justin Maple <justinamaple@gmail.com>
2020-04-19 12:17:08 -0700

    Added Number of 1 bits

---
+++ Leetcode/Ruby/number_of_1_bits.rb
@@ -0,0 +1,14 @@
+# @param {Integer} n, a positive integer
+# @return {Integer}
+def hamming_weight(n)
+  bin = to_binary_str(n)
+  count_ones(bin)
+end
+
+def to_binary_str(int)
+  int.to_s(2)
+end
+
+def count_ones(bin)
+  bin.chars.reduce(0) { |sum, bit| bit == '1' ? sum + 1 : sum }
+end
