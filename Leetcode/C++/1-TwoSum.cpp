f66da7175ed20be9154eebe32ac55dc727640810
Justin Maple <justinamaple@gmail.com>
2023-01-19 15:51:29 -1000

    Create 1-TwoSum.cpp
    Started studying up again for Game Dev and thought it would be a good idea to get some C++ practice in.

---
+++ Leetcode/C++/1-TwoSum.cpp
@@ -0,0 +1,73 @@
+// Source:
+//   https://leetcode.com/problems/two-sum/
+// Solution:
+//   This problem is a little more complex since the vector given is not sorted. To avoid
+//   going through every single permutation and taking O(n^2), I have chosen to sort the list first.
+//   After creating a sorted list, you can do a binary search, pulling the head and tail in towards
+//   the center, requiring O(n) to search + the sort time, which I will assume is O(n log n). Once
+//   the values are found in the sorted list I will then have to search the unsorted list to find
+//   their original indices.
+//
+//   This whole process could be sped up by creating a lookup table while sorting the array. Then
+//   once the values are found in the sorted array, you could find them in constant time rather than
+//   having to search the list once again.
+// Time Complexity:
+//   O(n) = O(n + n log n), Multiple searches plus a sort
+// Space Complexity:
+//   O(n), one extra vector will be generated plus some helper variables
+//   where n is equal to the size of the given vector nums.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::vector<int> sorted = nums;
+        std::sort(sorted.begin(), sorted.end());
+        
+        int head = 0;
+        int tail = sorted.size() - 1;
+        int sum = sorted[head] + sorted[tail];
+        
+        //Head and Tail will be the indices of the values from the sorted array
+        while(sum != target) {
+            if (head >= tail)
+                return {-1, -1};
+            else if(sum > target) {
+                tail--;
+            } 
+            else { //sum < target
+                head++;
+            }
+
+            sum = sorted[head] + sorted[tail];
+        }
+
+        cout << "headVal = " << sorted[head] << "\n";
+        cout << "tailVal = " << sorted[tail] << "\n";
+
+        bool headFound = false;
+        bool tailFound = false;
+        int headsIndex = -1;
+        int tailsIndex = -1;
+
+        for(int i = 0; i < nums.size(); i++) {
+            cout << i << "\n";
+            if(headFound && tailFound) {
+                cout << "head and tail found, returning early" << "\n";
+                return {headsIndex, tailsIndex};
+            }
+
+            if(nums[i] == sorted[head] && !headFound) {
+                cout << "head found" << "\n";
+                headsIndex = i;
+                headFound = true;
+            }
+            else if(nums[i] == sorted[tail] && !tailFound) {
+                cout << "tail found" << "\n";
+                tailsIndex = i;
+                tailFound = true;
+            }
+        }
+    
+        return {headsIndex, tailsIndex};
+    }
+};
