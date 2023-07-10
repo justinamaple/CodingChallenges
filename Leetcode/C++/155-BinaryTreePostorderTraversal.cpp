5ae8acb429c1c3e497b5544c31e9471b4f463eca
Justin Maple <justinamaple@gmail.com>
2023-07-10 14:37:38 -0700

    Added comment to BTPT

--- Leetcode/C++/155-BinaryTreePostorderTraversal.cpp
+++ Leetcode/C++/155-BinaryTreePostorderTraversal.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Source:
+ *   https://leetcode.com/problems/binary-tree-postorder-traversal/
+ * Solution:
+ *   Simple solution using recursion for a standard tree traversal. Traverses
+ *   as far left, then right, and finally when recursing back up adds the values
+ *   to the vector.
+ * 
+ *   Uses a reference, &, to affect the original vector directly. This saves time 
+ *   by not having to pass around a copy of the vector each time.
+ * 
+ *   Time/space could potentially be improved by checking if the left/right are
+ *   empty before calling the function to return. Depends on how things are implemented
+ *   if this function was called heavily it would be worth checking in order to optimize
+ * Time Complexity:
+ *   O(n), where n is the amount of TreeNode's
+ * Space Complexity:
+ *   O(1), no extra data sctructures are created
+ */
 #include <vector>
 class Solution {
 private:
