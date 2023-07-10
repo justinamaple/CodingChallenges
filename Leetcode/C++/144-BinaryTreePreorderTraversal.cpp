6d8839820f6d8350edb37f28b196a596adfe5bc2
Justin Maple <justinamaple@gmail.com>
2023-07-10 14:30:25 -0700

    Updated comment for BTIOT

--- Leetcode/C++/144-BinaryTreePreorderTraversal.cpp
+++ Leetcode/C++/144-BinaryTreePreorderTraversal.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Source:
+ *   https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * Solution:
+ *   Simple solution using recursion for a standard tree traversal. Adds each value
+ *   the first time it visits, as it goes down left. Then right one, and continues
+ *   to the left.
+ * 
+ *   Uses a reference, &, to affect the original vector directly. This saves time 
+ *   by not having to pass around a copy of the vector each time.
+ * 
+ *   Time/space could potentially be improved by checking if the left/right are
+ *   empty before calling the function to return. Depends on how things are implemented
+ *   if this function was called heavily it would be worth checking in order to optimize
+ * Time Complexity:
+ *   O(n), where n is the amount of TreeNode's
+ * Space Complexity:
+ *   O(1), no extra data sctructures are created
+ */
 #include <vector>
 class Solution {
 private:
