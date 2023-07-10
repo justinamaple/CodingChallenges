55ff94d2fb0cc87476c1374097b9628d38f8abbb
Justin Maple <justinamaple@gmail.com>
2023-07-10 13:28:24 -0700

    Added comments to BinaryTreeInOrderTraversal

--- Leetcode/C++/94-BinaryTreeInOrderTraversal.cpp
+++ Leetcode/C++/94-BinaryTreeInOrderTraversal.cpp
@@ -1,3 +1,22 @@
+/* 
+ * Source:
+ *   https://leetcode.com/problems/binary-tree-inorder-traversal/
+ * Solution:
+ *   Simple solution using recursion for a standard tree traversal. First navigates
+ *   the left side tree as far as possible, then adds that node. Then it goes right
+ *   and one, then again tries to go as left as possible. Repeat. 
+ * 
+ *   Uses a reference, &, to affect the original vector directly. This saves time 
+ *   by not having to pass around a copy of the vector each time.
+ * 
+ *   Time/space could potentially be improved by checking if the left/right are
+ *   empty before calling the function to return. Depends on how things are implemented
+ *   if this function was called heavily it would be worth checking in order to optimize
+ * Time Complexity:
+ *   O(n), where n is the length of the longest string.
+ * Space Complexity:
+ *   O(1), no extra data sctructures are created, just some new pointers and nodes.
+ */
 #include <vector>
 class Solution {
 private:
